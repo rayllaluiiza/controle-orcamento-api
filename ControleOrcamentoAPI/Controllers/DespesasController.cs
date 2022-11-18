ad7d6421a08941e2abb327ea6a86c0c80786bae7
Raylla Luiza <raylla_op12@hotmail.com>
2022-11-18 19:21:46 -0300

    Adicionar arquivos de projeto.

---
+++ ControleOrcamentoAPI/Controllers/DespesasController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleOrcamentoAPI.Controllers
+{
+    public class DespesasController : ControllerBase
+    {
+    }
+}
---
+++ ControleOrcamentoAPI/Controllers/ReceitasController.cs
@@ -0,0 +1,16 @@
+﻿using ControleOrcamentoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleOrcamentoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReceitasController : ControllerBase
+    {
+        [HttpPost]
+        public void add([FromBody] Receitas receita)
+        {
+
+        }
+    }
+}
